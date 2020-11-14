560a00f2ace85d6eaef57f010b4a3462b15637b7
LouisDo <louisdo15111000@gmail.com>
2020-11-14 15:41:30 +0700

    Updated code

--- src/model_trainer/train.py
+++ src/model_trainer/train.py
@@ -54,10 +54,10 @@ class ModelTrainer:
     def weights_init(m):
         classname = m.__class__.__name__
         if classname.find('Conv') != -1:
-            nn.init.normal_(m.weight.data, 0.0, 0.02)
+            torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
         elif classname.find('BatchNorm') != -1:
-            nn.init.normal_(m.weight.data, 1.0, 0.02)
-            nn.init.constant_(m.bias.data, 0)
+            torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
+            torch.nn.init.constant_(m.bias.data, 0)
 
     def _update_discriminator(self, 
                               images: torch.tensor,
