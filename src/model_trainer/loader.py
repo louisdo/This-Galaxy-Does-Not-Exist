23d0821b5539297675042fec29873988465ae598
LouisDo <louisdo15111000@gmail.com>
2021-05-08 15:10:50 +0700

    Fixed bugs

--- src/model_trainer/loader.py
+++ src/model_trainer/loader.py
@@ -43,27 +43,22 @@ class StanfordCarDataset(Dataset):
         self.im_files = im_files
 
         mat_file = scipy.io.loadmat(labels_file)["annotations"][0]
-        self.labels = {os.path.join(data_folder, mat_file[index][-1]) : mat_file[index][-2] for index in range(len(mat_file))}
+        self.labels = {os.path.join(data_folder, mat_file[index][-1].item()) : mat_file[index][-2].item() - 1 for index in range(len(mat_file))}
 
         # The mean and std is from imagenet
         mean = [123.675, 116.28 , 103.53]
         std = [58.395, 57.12 , 57.375]
         self.transform = transforms.Compose([transforms.ToTensor(),
                                              transforms.Normalize(mean=mean, std=std),
-                                             transforms.Resize(imsize)])
+                                             transforms.Resize((imsize, imsize))])
 
     def load_image(self, im_file):
         return Image.open(im_file)
-
-    def generate_one_hot_label(self, label):
-        res = np.zeros([196])
-        res[label - 1] = 1
-        return res.astype(np.int)
 
     def __len__(self) -> int:
         return len(self.im_files)
 
     def __getitem__(self, index) -> ("torch.tensor", "torch.tensor"):
         img = self.load_image(self.im_files[index])
-        label = self.generate_one_hot_label(self.labels[self.im_files[index]])
-        return self.transform(img), torch.from_numpy(label)
+        label = self.labels[self.im_files[index]]
+        return self.transform(img), torch.tensor(label)
